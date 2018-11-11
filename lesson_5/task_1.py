fce2ce386a655c03daf38785a4b3eda6482ae2a5
Alex Bakulin <mirmaker6@yandex.ru>
2018-11-11 19:34:33 +0300

    Дз для пятого занятия

---
+++ lesson_5/task_1.py
@@ -0,0 +1,33 @@
+# 1. Пользователь вводит данные о количестве предприятий, их наименования и прибыль
+# за 4 квартала для каждого предприятия. Программа должна определить среднюю прибыль
+# (за год для всех предприятий) и вывести наименования предприятий, чья прибыль выше
+# среднего и отдельно вывести наименования предприятий, чья прибыль ниже среднего.
+#
+# Примечание: 4 квартала - это 4 разных прибыли ;-)
+
+from collections import namedtuple
+from statistics import mean
+
+New_Company = namedtuple('New_Company', 'name profit_list avg')
+
+lst = []
+for i in range(int(input('Введите количество компани '))):
+    arg = input('Введите в строку имя и поквартальную прибыль через пробел:\n').split()
+    lst.append(New_Company(arg[0], arg[1:], mean(map(int, arg[1:3]))))
+
+avg = mean([i.avg for i in lst])
+
+for i in lst:
+    print(f'Компания: {i.name} \t\tСредняя прибыль за год: {i.avg}')
+
+print('_' * 70, '\n')
+
+print('Компании с прибылью меньше средней:')
+for i in lst:
+    if i.avg < avg:
+        print(i.name)
+
+print('Компании с прибылью больше средней:')
+for i in lst:
+    if i.avg >= avg:
+        print(i.name)

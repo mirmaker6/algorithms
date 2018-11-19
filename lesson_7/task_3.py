8a15607be36d838ea9be353e7ed98b4b61296a08
Alex Bakulin <mirmaker6@yandex.ru>
2018-11-19 15:03:22 +0300

    Дз для 7 занятия

---
+++ lesson_7/task_3.py
@@ -0,0 +1,63 @@
+# Массив размером 2m + 1, где m – натуральное число, заполнен случайным образом.
+#  Найти в массиве медиану. Медианой называется элемент ряда, делящий его на две
+#  равные части: в одной находятся элементы, которые не меньше медианы, в другой – не больше ее.
+
+import random
+
+LST_LEN = 5
+
+lst = [random.randint(0, 100) for _ in range(2 * LST_LEN + 1)]
+
+
+# К сожалению, не хавтило времени доделать до конца.
+# Функция неверно возвращет значение если медиана встречается в массиве больше одного раза :(
+def median(lst):
+    k = 0
+    min_val = min(lst)
+    max_val = max(lst)
+
+    while True:
+
+        min_lst = 0
+        max_lst = 0
+        var = lst[k]
+
+        if min_val <= var <= max_val:
+            for i in lst:
+                if i <= var:
+                    min_lst += 1
+                if i >= var:
+                    max_lst += 1
+
+        if min_lst == max_lst != 0:
+            return var
+        elif min_lst > max_lst:
+            max_val = var
+        elif min_lst < max_lst:
+            min_val = var
+
+        if len(lst)-1 > k:
+            k += 1
+        else:
+            return max_val
+
+
+# Гномья сортировка
+def gnome_sort(lst):
+    i = 1
+    while i < len(lst):
+
+        if lst[i - 1] <= lst[i]:
+            i += 1
+        else:
+            lst[i], lst[i - 1] = lst[i - 1], lst[i]
+            i -= 1
+            if i == 0:
+                i = 1
+
+    return lst
+
+
+print(f'Исходный массив:\n{lst}')
+print(f'Медиана по гномьей сортировке: {gnome_sort(lst)[len(lst) // 2]}')
+print(f'Медиана по моей сортировке: {median(lst)}')
